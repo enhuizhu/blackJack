1b3b92b0dbae39fd404c23d14366e452ca7f0ff4
rocabee-david <david@rocabee.com>
2015-12-26 17:33:47 +0000

    udpate

--- js/game.cards.js
+++ js/game.cards.js
@@ -10,10 +10,12 @@ game.cards={
    /**
    * generate card for user
    **/
-   generateUserCard:function(n){
-       this.generateCardDom(n,"player",jQuery(".gameStage"));     
-       var cardDom = jQuery("[data-playerCardNumber="+n+"]");
-       var position = this.getUserCardPosition(n,"player");
+   generateUserCard:function(n, cardValue){
+       this.generateCardDom(n,"player",jQuery(".gameStage"), cardValue);
+       
+       var cardDom = jQuery("[data-playerCardNumber="+n+"]"),
+       	   position = this.getUserCardPosition(n,"player");
+       
        setTimeout(function(){
 	     cardDom.addClass("userFirtCard").css({"left":position.x+"px","top":position.y+"px"});
 		 /**
@@ -29,20 +31,22 @@ game.cards={
    * function to get user card position
    **/
    getUserCardPosition:function(n,who){
-      var firstCardPositionX=453;	
-	  var firstCardPositionY=who=="player"?274:38;
-      var distanceBetweenCards=who=="player"?42:30; 
-      var posX=firstCardPositionX+(n-1)*distanceBetweenCards;
+	   var firstCardPositionX=453,	
+		   firstCardPositionY=who=="player"?380:143,
+	       distanceBetweenCards=who=="player"?42:30,
+	       posX=firstCardPositionX+(n-1)*distanceBetweenCards;
+	  
 	  return {x:posX,y:firstCardPositionY}; 
    },   
-  
    /**
    * function to generate computer card
    **/
-   generateComputerCard:function(n){
-       this.generateCardDom(n,"computer",jQuery(".gameStage"));     
-       var cardDom = jQuery("[data-computerCardNumber="+n+"]");
-       var position = this.getUserCardPosition(n,"computer");
+   generateComputerCard:function(n, cardValue){
+       this.generateCardDom(n,"computer",jQuery(".gameStage"), cardValue);
+
+       var cardDom = jQuery("[data-computerCardNumber="+n+"]"),
+           position = this.getUserCardPosition(n,"computer");
+       
        setTimeout(function(){
 	     cardDom.addClass("computerCard").css({"left":position.x+"px","top":position.y+"px"});
 		 /**
@@ -53,25 +57,25 @@ game.cards={
 		 });
 	   },100);
     },
-  
- 
    /**
    * function to generate card dom
    **/
-   generateCardDom:function(n,who,parentDom){
+   generateCardDom:function(n,who,parentDom,cardValue){
       if(who=="player"){
 	     var attr='data-playerCardNumber="'+n+'"';
 	  }else{
 	     var attr='data-computerCardNumber="'+n+'"';
 	  }
+      
       var cardDom='<div class="flip-container cardInStock" '+attr+'>'+
 	   '<div class="flipper">'+
 		'<div class="front">'+
 		'</div>'+
-		'<div class="back bh1">'+
+		'<div class="back '+cardValue+'">'+
 		'</div>'+
 	  '</div>'+
       '</div>';
+      
       parentDom.append(cardDom);
     },
 	
@@ -82,9 +86,10 @@ game.cards={
 	    /**
 		* should get all the user card dom
 		**/
-		var selector=who=="player"?"[data-playerCardNumber]":"[data-computerCardNumber]"
-		var userCards = jQuery(selector);
-		var timeDistance=500;
+		var selector=who=="player"?"[data-playerCardNumber]":"[data-computerCardNumber]",
+			userCards = jQuery(selector),
+		 	timeDistance=500;
+		
 		for(var i=0;i<userCards.length;i++){
 		    var cardDom=jQuery(userCards.get(i));
 		    this.removeSinglCardDom(cardDom,i);	
